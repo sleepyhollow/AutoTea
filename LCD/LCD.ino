be584a44c2e87558ffc463dc5a963f853f43f93c
Thanh Vuong <thanh.vuong@yahoo.com>
2018-03-16 20:08:59 -0400

    changed pins and loop
    following hello world guide on arduino site, still doesn't work

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -3,9 +3,10 @@
 // Display pins to board pins
 const int LCD_RS = 12,
           LCD_EN = 11,
-          LCD_D4 = 10,
-          LCD_D6 = 8,
-          LCD_D7 = 7;
+          LCD_D4 = 5,
+          LCD_D5 = 4,
+          LCD_D6 = 3,
+          LCD_D7 = 2;
 
 // Initialize display
 LiquidCrystal lcd(LCD_RS,
@@ -19,13 +20,19 @@ void setup() {
   // Set display's number of columns and rows
   lcd.begin(16, 2);
 
+  
+  lcd.print("fml");
 }
 
 void loop() {
   // Turn on display
-  lcd.display();
+  //lcd.display();
 
   // Print a message to display
-  lcd.print("fml");
-  
+  //lcd.print("fml");
+
+  // Set cursor to column and row number
+  lcd.setCursor(0, 1);
+  // Print numbe of secs since reset
+  lcd.print(millis() / 1000);
 }
